1d8d3b58779368cdeb9746f1b94d625c3d711c56
lucahan <lucahan@foxmail.com>
2021-07-04 21:44:14 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="container" class="box">
+        <div id="cesiumContainer"></div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {},
+        mounted() {
+            this.init()
+        },
+        methods: {
+            init() {
+                let Cesium = this.cesium
+                let viewer = new Cesium.Viewer('cesiumContainer');
+                viewer._cesiumWidget._creditContainer.style.display = "none";// 隐藏版权
+
+                //矢量底图
+                viewer.imageryLayers.addImageryProvider(
+                    new this.cesium.WebMapTileServiceImageryProvider({
+                        url: "http://t0.tianditu.com/vec_w/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=vec&tileMatrixSet=w&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default.jpg&tk=719fbabce3f84fb70bb9884d7df106ee",
+                        layer: "tdtImgBasicLayer",
+                        style: "default",
+                        format: "image/jpeg",
+                        tileMatrixSetID: "GoogleMapsCompatible",
+                        show: false
+                    })
+                );
+                //中文地名注记
+                viewer.imageryLayers.addImageryProvider(
+                    new this.cesium.WebMapTileServiceImageryProvider({
+                        url: "http://t0.tianditu.com/cia_w/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=cia&tileMatrixSet=w&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default.jpg&tk=719fbabce3f84fb70bb9884d7df106ee",
+                        layer: "tdtAnnoLayer",
+                        style: "default",
+                        format: "tiles",
+                        tileMatrixSetID: "GoogleMapsCompatible",
+                        show: true
+                    })
+                );
+
+                // 将三维球定位到中国
+                viewer.camera.flyTo({
+                    destination: Cesium.Cartesian3.fromDegrees(112.96, 28.20, 57850)
+                });
+            }
+        }
+
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    html,
+    body,
+    #cesiumContainer {
+        width: 100%;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+    }
+
+    .box {
+        height: 100%;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+
+Vue.config.productionTip = false;
+
+//引入cesium相关文件
+const cesium = require('cesium/Cesium');
+const widgets= require('cesium/Widgets/widgets.css');
+
+Vue.prototype.cesium = cesium
+Vue.prototype.widgets = widgets
+
 
 new Vue({
   render: h => h(App),
